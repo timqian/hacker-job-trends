127354939f5e33f3561dc78d2608e3278eff86a4
Yann Duval <me@yannduval.de>
2019-02-27 20:38:59 +0100

    Using cached data if available and centralizing paths

--- src/api/fetchNewlyAddedHTMLs.js
+++ src/api/fetchNewlyAddedHTMLs.js
@@ -4,16 +4,20 @@ const axios = require('axios')
 
 const apiBase = 'https://hacker-news.firebaseio.com/v0/item'
 
-async function fetchNewlyAddedHTMLs () {
-  const baseFolder = path.join(__dirname, '../../assets/api')
-  const str = fs.readFileSync(path.join(__dirname, '../../HN-who-is-hiring-monthly.md'), 'utf-8')
+const basePath = path.join(__dirname, '../..')
+const pathToDatedLinks = path.join(basePath, '/HN-who-is-hiring-monthly.md')
+const pathToRawData = path.join(basePath, 'assets/api', '/raw')
+const pathToProcessedData = path.join(basePath, 'assets/api', '/processed')
 
-  const linkArr = str.split('\n').map(datedLink => ({
+async function fetchNewlyAddedHTMLs () {
+  const datedLinks = fs.readFileSync(pathToDatedLinks, 'utf-8')
+
+  const linkArr = datedLinks.split('\n').map(datedLink => ({
     month: datedLink.split(': ')[0].slice(2),
     link: datedLink.split(': ')[1]
   }))
 
-  const fetchedStories = fs.readdirSync(path.join(baseFolder, '/processed'))
+  const fetchedStories = fs.readdirSync(pathToProcessedData)
   const storyIds = fetchedStories.map(fileName => fileName.split('-combined.json')[0])
 
   for (const linkObj of linkArr) {
@@ -21,10 +25,18 @@ async function fetchNewlyAddedHTMLs () {
       const storyId = linkObj.link.substring('https://news.ycombinator.com/item?id='.length)
       console.log('Fetching jobs of:', linkObj.month)
 
-      const res = await axios.get(`${apiBase}/${storyId}.json`)
-      fs.writeFileSync(path.join(baseFolder, `/raw/${storyId}.json`), JSON.stringify(res.data))
+      let res;
+      if (fs.existsSync(path.join(pathToRawData, `/${storyId}.json`))) {
+        console.log('using cached story');
+        res = fs.readFileSync(path.join(pathToRawData, `/${storyId}.json`))
+        res = JSON.parse(res)
+      } else {
+        res = await axios.get(`${apiBase}/${storyId}.json`)
+        res = res.data
+        fs.writeFileSync(path.join(pathToRawData, `/${storyId}.json`), JSON.stringify(res))
+      }
 
-      const storyFolder = path.join(baseFolder, `/raw/${storyId}/`)
+      const storyFolder = path.join(pathToRawData, `/${storyId}/`)
       if (!fs.existsSync(storyFolder)) {
         fs.mkdirSync(storyFolder)
       }
@@ -33,26 +45,28 @@ async function fetchNewlyAddedHTMLs () {
       const kidIds = fetchedKids.map(fileName => fileName.split('.json')[0])
 
       let promises = []
-      res.data.kids.forEach((value, idx) => {
+      res.kids.forEach((value, idx) => {
         if (!kidIds.includes(`${value}`)) {
           promises.push(
             axios.get(`${apiBase}/${value}.json`).then((response) => {
-              fs.writeFileSync(path.join(baseFolder, `/raw/${storyId}/${value}.json`), JSON.stringify(response.data))
+              fs.writeFileSync(path.join(pathToRawData, `/${storyId}/${value}.json`), JSON.stringify(response.data))
               return response.data
             })
           )
         } else {
-          const d = fs.readFileSync(path.join(baseFolder, `/raw/${storyId}/${value}.json`))
+          const d = fs.readFileSync(path.join(pathToRawData, `/${storyId}/${value}.json`))
           promises.push(Promise.resolve(JSON.parse(d)))
         }
       })
 
       let output = { comments: [] }
-      await Promise.all(promises).then((comment) => {
-        output.comments.push(comment)
+      await Promise.all(promises).then((result) => {
+        result.forEach(comment => {
+          output.comments.push(comment)
+        })
       })
 
-      fs.writeFileSync(path.join(baseFolder, `/processed/${linkObj.month}-combined.json`), JSON.stringify(output))
+      fs.writeFileSync(path.join(pathToProcessedData, `/${linkObj.month}-combined.json`), JSON.stringify(output))
       console.log('Fetching done:', linkObj.month)
     }
   }
